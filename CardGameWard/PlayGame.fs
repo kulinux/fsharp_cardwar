9eb95b562a06fdaa9ca1cfb73528b6e20240dbdb
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2023-11-25 07:46:29 +0100

    Refactor

--- CardGameWard/PlayGame.fs
+++ CardGameWard/PlayGame.fs
@@ -17,49 +17,66 @@ module PlayGame =
         | TurnResult of TurnResult
         | GameResult of GameResult
 
-    let turnResultCard (card1: Card, card2: Card) : TurnResult =
+    let turnResultCard (card1: Card) (card2: Card) : TurnResult =
         if card1.number > card2.number then Player1Win
         elif card1.number < card2.number then Player2Win
         else Equal
 
-    let turnResult (game: Game) : TurnOrGameResult =
-        match (game.player1.cards, game.player2.cards) with
-        | (player1Head :: _), (player2Head :: _) -> TurnResult(turnResultCard (player1Head, player2Head))
-        | [], _ -> GameResult(Player2WinMatch)
-        | _, [] -> GameResult(Player1WinMatch)
+    let getCards (n: int) (card: Card list) : Card list * Card list =
+        if card.Length >= n then
+            (card |> List.take n, card |> List.skip n)
+        else
+            raise (OuterError($"Player win {n}"))
 
-    let war(game: Game): Game =
-        let play1war = game.player1.cards |> List.head
-        let play2war = game.player2.cards |> List.head
-        let play1down = game.player1.cards |> List.take 4 |> List.skip 1
-        let play2down = game.player2.cards |> List.take 4 |> List.skip 1
-        let cardToPlay1 = game.player1.cards |> List.skip 4 |> List.head
-        let cardToPlay2 = game.player2.cards |> List.skip 4 |> List.head
-        let rest1 = game.player1.cards |> List.skip 5
-        let rest2 = game.player2.cards |> List.skip 5
-        match turnResultCard (cardToPlay1, cardToPlay2) with
-            | Player1Win ->
-                { player1 = {cards = [play2war] @ play2down @ [ cardToPlay2 ] @ [play1war] @ play1down @ [ cardToPlay1 ] @ rest1 }
-                  player2 = { cards = rest2 }}
-            | Player2Win ->
-                { player1 = {cards = rest1}
-                  player2 = { cards = [play1war] @ play1down @ [ cardToPlay1 ] @ [play2war] @ play2down @ [ cardToPlay2 ] @ rest2 } }
-            | _ -> raise(OuterError("Not implemented")) 
+
+    let getCards2(a: int)(b: int) (card1: Card list) (card2: Card list): Card list * Card list * Card list * Card list =
+        let card1Cards = card1 |> getCards a
+        let card2Cards = card2 |> getCards b
+        (fst card1Cards, snd card1Cards, fst card2Cards, snd card2Cards)
+
+    let warPack (player: Player): Card list * Card list * Card list * Card list =
+         player.cards |> getCards 4 ||> getCards2 1 1 
+        
+
+    let war (game: Game) : Game =
+        let play1war, play1down, cardToPlay1, rest1 = warPack game.player1
+        let play2war, play2down, cardToPlay2, rest2 = warPack game.player2
+
+        match turnResultCard cardToPlay1.Head cardToPlay2.Head with
+        | Player1Win ->
+            { player1 =
+                { cards =
+                    play2war
+                    @ play2down
+                    @ cardToPlay2
+                    @ play1war
+                    @ play1down
+                    @ cardToPlay1
+                    @ rest1 }
+              player2 = { cards = rest2 } }
+        | Player2Win ->
+            { player1 = { cards = rest1 }
+              player2 =
+                { cards =
+                    play1war
+                    @ play1down
+                    @ cardToPlay1
+                    @ play2war
+                    @ play2down
+                    @ cardToPlay2
+                    @ rest2 } }
+        | _ -> raise (OuterError("Not implemented"))
 
 
     let playOneTurn (game: Game) : Game =
-        let player1Head = game.player1.cards.Head
-        let player1Tail = game.player1.cards.Tail
+        let (player1Head, player1Tail) = game.player1.cards |> getCards 1
+        let (player2Head, player2Tail) = game.player2.cards |> getCards 1
 
-        let player2Head = game.player2.cards.Head
-        let player2Tail = game.player2.cards.Tail
-
-        match turnResult game with
-        | TurnResult Player1Win ->
-            { player1 = { cards = [ player1Head ] @ player1Tail @ [ player2Head ] }
+        match turnResultCard player1Head.Head player2Head.Head with
+        | Player1Win ->
+            { player1 = { cards = player1Head @ player1Tail @ player2Head }
               player2 = { cards = player2Tail } }
-        | TurnResult Player2Win ->
+        | Player2Win ->
             { player1 = { cards = player1Tail }
-              player2 = { cards = [ player2Head ] @ player2Tail @ [ player1Head ] } }
-        | TurnResult Equal -> war game
-        | GameResult _ -> raise(OuterError("Player win"))
+              player2 = { cards = player2Head @ player2Tail @ player1Head } }
+        | Equal -> war game
